10b77573b61532272f16f6e4ba3d27ca46730aef
kllimchuuk <benggein05@gmail.com>
2025-03-28 14:31:30 +0100

    Updates lesson_second.py

---
+++ lesson_second.py
@@ -0,0 +1,162 @@
+#number = int(input('Enter a number:'))
+
+#x = 1
+#total = 0
+
+# start of the if statement
+
+# if x != 0:
+#   total += x
+
+#    print(total)
+#end of the if statement
+
+#print("This is always executed.")
+
+#number = int(input("Enter a number: "))
+
+#if number != 0:
+#   print("Positive number")
+#else:
+#    print("Not a positive number")
+
+#print("This statement always executes")
+
+#number = 88
+
+#if number > 0:
+#   print("Positive number")
+
+#elif number < 0:
+#    print("Negative number")
+
+#else:
+#    print("Zero")
+#
+#print("This statement is always executed")
+
+#number = 0
+
+#if number >= 0:
+#    if number == 0:
+#       print("Number is 0")
+#    else:
+#        print("Number is positive")
+#
+#else:
+#    print("Number is negative")
+#
+#
+# if score > 50:
+#        return "Passed"
+#
+#   else
+#   return "Failed"
+
+#print(pass_fail(50))
+
+#FOR
+
+#languages = ["swift", "Python", "Go"]
+
+#for lang in languages:
+#    print(lang)
+#    print("------")
+#
+#print("Last statement")
+
+#language ="Python"
+
+#for x in language:
+#    print(x)
+
+#values = range(0, 4)
+
+#for i in range(0, 4):
+#   print(i)
+
+#languages = ["Swift", "Python", "Go", "C++"]
+
+#for lang in languages:
+#   if lang == "Go":
+#        #continue та break
+#    print(lang)
+
+#attributes = ["Electric", "Fast"]
+#cars = ["Tesla", "Porsche", "Mercedes"]
+
+#for attribute in attributes:
+#    for car in cars:
+#        print(attribute, car)
+
+#    print("-----")
+
+#for _ in range(0, 4):
+#    print("HI")
+#def factorial(n):
+#    result = 1
+#    for i in range(2, n + 1): # n + 1 це як range(2, 6)
+#        result *= i
+#    return result
+
+#print(factorial(5))
+#print(factorial(0))
+#print(factorial(9))
+
+# WHILE
+
+number = 1
+
+#while number <= 3:
+#    print(number)
+#    number = number + 1
+
+#number = int(input("Enter a number: "))
+
+#while number != 0 and number > 0:
+#    print(f"You entered {number}.")
+#    number = int(input("Enter a number: "))
+
+#print("The end.")
+
+#def fibonacci_sequence(limit):
+#    if limit < 0:
+#        return []
+
+#    sequence = [0, 1]
+#    while limit > 0:
+#        next_value = sequence[-1] + sequence[-2]
+#        if next_value >= limit:
+#            break
+#        sequence.append(next_value)
+
+#    return sequence
+
+#print(fibonacci_sequence(22))
+
+#Break
+
+#for i in range(5):
+#    if i == 3:
+#        break
+#    print(i)
+
+#for i in range(5):
+#    if i == 3:
+#        continue
+#    print(i)
+
+#def sum_greater_than(numbers, n):
+#    return sum(num for num in numbers if num > n)
+
+#print(fibonacci_sequence(22))
+#print(sum_greater_than([1, 2, 3, 4, 5], 3))
+
+#n = 10
+
+# PASS
+#if n > 10:
+#    pass #
+
+#print('Hello')
+
